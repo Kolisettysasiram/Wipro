4edd56ab31995a34c79eb6447c24ba3c9531fd0d
User <User@WINDOWS-OSPQPBQ>
2020-06-13 19:49:44 +0530

    Example 4

---
+++ Milestone1/src/com/wipro/arrays/Ex4.java
@@ -0,0 +1,17 @@
+package com.wipro.arrays;
+
+public class Ex4 {
+
+	public static void main(String[] args) {
+		int a[]= {65,69,36,37,100};
+		for(int i=0;i<a.length;i++)
+		{
+			char c=(char)a[i]; 
+			System.out.print(c +" ");
+
+		}
+				
+
+	}
+
+}
