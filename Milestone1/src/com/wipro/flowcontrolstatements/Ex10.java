9c257dca263725c1d9b56d9b23df3ba527996f28
User <User@WINDOWS-OSPQPBQ>
2020-06-13 19:38:43 +0530

    Example 10

---
+++ Milestone1/src/com/wipro/flowcontrolstatements/Ex10.java
@@ -0,0 +1,16 @@
+package com.wipro.flowcontrolstatements;
+
+public class Ex10 {
+
+	public static void main(String[] args) {
+		int i=10;
+		for(i=1;i<=10;i++) {
+			System.out.print(i);
+			if(i==10)break;
+			else
+				System.out.print("\t");
+		}
+		
+	}
+
+}
