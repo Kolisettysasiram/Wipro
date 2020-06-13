1ac3fd6ea43f44c4f175b4d5f7d6a98b63bb7f4b
User <User@WINDOWS-OSPQPBQ>
2020-06-13 19:26:25 +0530

    Example 2

---
+++ Milestone1/src/com/wipro/flowcontrolstatements/Ex2.java
@@ -0,0 +1,14 @@
+package com.wipro.flowcontrolstatements;
+
+public class Ex2 {
+
+	public static void main(String[] args) {
+		int a=Integer.parseInt(args[0]);
+		if(a%2==0)
+			System.out.println("even");
+		else
+			System.out.println("odd");
+
+	}
+
+}
