9dd6242d7c9b66ca2afe492b5f05cbb8ea5fa73f
User <User@WINDOWS-OSPQPBQ>
2020-06-24 08:57:01 +0530

    Ex 2 person

---
+++ Milestone1/src/com/wipro/inheritance/person.java
@@ -0,0 +1,19 @@
+package com.wipro.inheritance;
+
+public class person {
+
+		String name;
+		person( String n)
+		{
+			name=n;
+		}
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		
+		
+	}
+
