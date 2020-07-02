a60acc88a8f3990bd42542b03dfd004b6af4257f
User <User@WINDOWS-OSPQPBQ>
2020-07-02 11:25:13 +0530

    Example 1

---
+++ Milestone1/src/com/wipro/packages/Ex1.java
@@ -0,0 +1,13 @@
+package com.wipro.packages;
+
+import testpackage.Foundation;
+
+public class Ex1 {
+
+	public static void main(String[] args) {
+		Foundation f = new Foundation();
+        f.Var4 = 5;
+		System.out.println(f.Var4);
+	}
+
+}
